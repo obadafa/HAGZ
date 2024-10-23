e2d50574663dec0f04797f549a13b31674ca1974
obada <34578532+obadafa@users.noreply.github.com>
2024-10-23 03:42:25 +0300

    Football Field Class

---
+++ football_field.py
@@ -0,0 +1,39 @@
+from flask import render_template
+from talk_to_database import *
+from talk_to_text_file import *
+
+class FootballField:
+    name = ""
+    user_id = ""
+    location = ""
+    map_link = ""
+    image = ""
+
+    # Parameterized constructor.
+    def __init__(self, name, user_id, location, map_link, image):
+        self.name = name
+        self.admin_id = user_id
+        self.location = location
+        self.map_link = map_link
+        self.image = image
+
+    # Add football field to database.        
+    def set_football_field(self):
+        # Check if Football Field Name Unique.
+        football_fields_array = get_batabase(f"SELECT name FROM HAGZ.football_field;")
+        for football_field_name in football_fields_array:
+            if self.name == football_field_name[0]:
+                return render_template('add_football_field.html', message="Football Field Name MUST be Unique")
+        
+        #Insert the football field after checking.
+        query = f"INSERT INTO HAGZ.football_field (admin_id, name, location, map_link, image) VALUES (%s, %s, %s, %s, %s);"
+        values = (self.admin_id, self.name, self.location, self.map_link, self.image)
+        set_batabase(query, values)
+        football_fields_array = get_batabase(f"SELECT * FROM HAGZ.football_field WHERE admin_id = {self.admin_id};")
+        return render_template('my_football_fields.html', football_fields_array = football_fields_array)
+
+# Retrieve all information about the football field.    
+def get_football_fields_db(admin_id):
+    query = f"SELECT * FROM HAGZ.football_field WHERE admin_id = {admin_id};"
+    football_fields = get_batabase(query)
+    return football_fields
